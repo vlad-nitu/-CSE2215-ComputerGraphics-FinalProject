216d0a38376b2cd7d9d1f25a8e36f27d62dad680
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-10-21 15:57:54 +0200

    Use phong specularties for getFinalColor

--- src/render.cpp
+++ src/render.cpp
@@ -3,31 +3,29 @@
 #include "light.h"
 #include "screen.h"
 #include <framework/trackball.h>
+
 #ifdef NDEBUG
 #include <omp.h>
 #endif
 
-glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, const Features& features, int rayDepth)
-{
+glm::vec3 getFinalColor(const Scene &scene, const BvhInterface &bvh, Ray ray, const Features &features, int rayDepth) {
     HitInfo hitInfo;
     if (bvh.intersect(ray, hitInfo, features)) {
 
         glm::vec3 Lo = computeLightContribution(scene, bvh, features, ray, hitInfo);
 
-        // Draw a debug ray with the color returned from the shading.
-        drawRay(ray, Lo);
 
         if (features.enableRecursive) {
             Ray reflection = computeReflectionRay(ray, hitInfo);
 
-            if (hitInfo.material.ks == glm::vec3{0, 0, 0} || rayDepth == 0)
-                return Lo;
-            else{
-                Lo = Lo + getFinalColor(scene, bvh, reflection, features, rayDepth - 1);
+            if (hitInfo.material.ks != glm::vec3{0, 0, 0} && rayDepth != 0) {
+                float angle = glm::dot(glm::normalize(ray.direction),  glm::normalize(reflection.direction));
+                Lo = Lo + hitInfo.material.ks * getFinalColor(scene, bvh, reflection, features, rayDepth - 1) * glm::cos(angle);
             }
             // drawRay(reflection, Lo); // debug only atm
         }
-        
+        // Draw a debug ray with the color returned from the shading.
+        drawRay(ray, Lo);
 
         // Set the color of the pixel.
         return Lo;
@@ -39,8 +37,8 @@ glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, co
     }
 }
 
-void renderRayTracing(const Scene& scene, const Trackball& camera, const BvhInterface& bvh, Screen& screen, const Features& features)
-{
+void renderRayTracing(const Scene &scene, const Trackball &camera, const BvhInterface &bvh, Screen &screen,
+                      const Features &features) {
     glm::ivec2 windowResolution = screen.resolution();
     // Enable multi threading in Release mode
 #ifdef NDEBUG
@@ -49,9 +47,9 @@ void renderRayTracing(const Scene& scene, const Trackball& camera, const BvhInte
     for (int y = 0; y < windowResolution.y; y++) {
         for (int x = 0; x != windowResolution.x; x++) {
             // NOTE: (-1, -1) at the bottom left of the screen, (+1, +1) at the top right of the screen.
-            const glm::vec2 normalizedPixelPos {
-                float(x) / float(windowResolution.x) * 2.0f - 1.0f,
-                float(y) / float(windowResolution.y) * 2.0f - 1.0f
+            const glm::vec2 normalizedPixelPos{
+                    float(x) / float(windowResolution.x) * 2.0f - 1.0f,
+                    float(y) / float(windowResolution.y) * 2.0f - 1.0f
             };
             const Ray cameraRay = camera.generateRay(normalizedPixelPos);
             screen.setPixel(x, y, getFinalColor(scene, bvh, cameraRay, features));
