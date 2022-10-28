cd5da766dc19a5806d7da200c28a5d54530d3ec2
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-10-28 15:09:55 +0200

    Bilinear Interpolation when not using Shading

--- src/shading.cpp
+++ src/shading.cpp
@@ -65,7 +65,10 @@ const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& 
         return shading;
     } else {
         if (features.enableTextureMapping && hitInfo.material.kdTexture) {
-            return acquireTexel(*hitInfo.material.kdTexture, hitInfo.texCoord, features);
+            if (features.extra.enableBilinearTextureFiltering)
+                return bilinearInterpolation(*hitInfo.material.kdTexture, hitInfo.texCoord, features); 
+            else
+                return acquireTexel(*hitInfo.material.kdTexture, hitInfo.texCoord, features);
         } else {
             return hitInfo.material.kd;
         }
