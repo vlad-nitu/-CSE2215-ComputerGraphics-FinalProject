8acb2d8eb0c40314d9e095b638b784701044a441
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-10-28 14:17:25 +0200

    Switched order for bilinear

--- src/texture.cpp
+++ src/texture.cpp
@@ -22,34 +22,21 @@ glm::vec3 acquireTexel(const Image& image, const glm::vec2& texCoord, const Feat
 
 glm::vec3 bilinearInterpolation (const Image& image, const glm::vec2& texCoord,  const Features& features)
 { 
-    glm::vec2 texelPos { (int)((image.width - 1) * texCoord.x), (int)((image.height - 1) * texCoord.y)};
+    glm::vec2 texelPos { (image.width - 1) * texCoord.x, (image.height - 1) * texCoord.y};
 
-    int clamp_texel_x = texelPos.x;
-    int clamp_texel_y = texelPos.y;
+    int clamp_texel_x = (int) texelPos.x;
+    int clamp_texel_y = (int) texelPos.y;
 
     glm::vec2 upper_left {clamp_texel_x, clamp_texel_y}; 
-    if (upper_left.x == image.width)
-        upper_left.x --;
-    if (upper_left.y == image.height)
-        upper_left.y --;
 
     glm::vec2 upper_right {upper_left.x - 1, upper_left.y};
-     if (upper_right.x == image.width)
-        upper_right.x --;
-     if (upper_right.y == image.height)
-        upper_right.y --;
+
 
     glm::vec2 lower_left {upper_left.x, upper_left.y - 1}; 
-    if (lower_left.x == image.width)
-        lower_left.x --;
-     if (lower_left.y == image.height)
-        lower_left.y --;
+
 
     glm::vec2 lower_right {upper_left.x - 1, upper_left.y - 1}; 
-    if (lower_right.x == image.width)
-        lower_right.x --;
-     if (lower_right.y == image.height)
-        lower_right.y --;
+
 
     glm::vec3 upper_left_color = image.pixels[upper_left.y * image.width + upper_left.x];
     glm::vec3 upper_right_color = image.pixels[upper_right.y * image.width + upper_right.x];
@@ -57,12 +44,12 @@ glm::vec3 bilinearInterpolation (const Image& image, const glm::vec2& texCoord, 
     glm::vec3 lower_right_color = image.pixels[lower_right.y * image.width + lower_right.x];
 
 
-    float alpha = texelPos.x - lower_left.x;
-    float betta = texelPos.y - lower_left.y;  
+    float alpha = texelPos.x - upper_left.x;
+    float betta = texelPos.y - upper_left.y;  
     
     glm::vec3 interpolated_texCoord = upper_left_color * glm::vec3{(1-alpha) * (1-betta)}
-     + upper_right_color  * glm::vec3{alpha * (1 - betta)} 
-     + lower_left_color * glm::vec3{(1-alpha) * betta} 
+     + lower_left_color  * glm::vec3{alpha * (1 - betta)} 
+     + upper_right_color * glm::vec3{(1-alpha) * betta} 
      + lower_right_color * glm::vec3{alpha * betta}; 
 
         return interpolated_texCoord; 
